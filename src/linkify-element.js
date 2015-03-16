bfa77c76c9f7551ee44b899b69cabf81db82be36
nfrasser <nfrasser@gmail.com>
2015-03-15 21:31:34 -0400

    Do not trip the text nodes
    This could cause some unexpected behaviour, leave everything as is.

--- src/linkify-element.js
+++ src/linkify-element.js
@@ -85,7 +85,7 @@ function linkifyElementHelper(element, opts, doc) {
 		case TXT_NODE:
 
 			let
-			str = childElement.nodeValue.replace(/^\s+|\s+$/g, ''), // trim
+			str = childElement.nodeValue,
 			tokens = tokenize(str);
 			children.push(...tokensToNodes(tokens, opts, doc));
 
