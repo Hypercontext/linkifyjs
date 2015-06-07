508443ab975165b5f3f64d6b0e37d385fe5a2406
nfrasser <nfrasser@gmail.com>
2015-06-06 19:22:30 -0700

    Updating .npmignore, better legacy template

--- templates/linkify-legacy.js
+++ templates/linkify-legacy.js
@@ -1,2 +1,2 @@
-;console.warn('dist/jquery.linkify.js is deprecated. Use linkify.js and linkify-jquery.js instead.');
+;typeof console !== 'undefined' && console.warn('dist/jquery.linkify.js is deprecated. Use linkify.js and linkify-jquery.js instead.');
 <%= contents %>
