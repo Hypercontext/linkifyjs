16dfc011dfebb371b1a52804f712751cb33cc04f
nfrasser <nfrasser@gmail.com>
2016-08-13 20:08:09 -0400

    Implementation with recursive support!
    Will transparently *not* linkify self-closing elements

--- src/linkify-react.js
+++ src/linkify-react.js
@@ -3,60 +3,89 @@ import * as linkify from './linkify';
 
 let options = linkify.options;
 
-var Linkify = React.createClass({
-	render() {
-		let str = this.props.data;
-		let opts = options.normalize(this.props);
+// Given a string, converts to an array of valid React components
+// (which may include strings)
+function stringToElements(str, opts) {
 
-		let tokens = linkify.tokenize(str);
-		let elements = [];
+	let tokens = linkify.tokenize(str);
+	let elements = [];
 
-		for (let i = 0; i < tokens.length; i++) {
-			let token = tokens[i];
-
-			if (token.type === 'nl' && opts.nl2br) {
-				elements.push(React.createElement('br'));
-				continue;
-			} else if (
-				!token.isLink ||
-				!options.resolve(opts.validate, token.toString(), token.type)
-			) {
-				// Regular text
-				elements.push(token.toString());
-				continue;
-			}
-
-			let href = token.toHref(opts.defaultProtocol);
-			let formatted = options.resolve(opts.format, token.toString(), token.type);
-			let formattedHref = options.resolve(opts.formatHref, href, token.type);
-			let attributesHash = options.resolve(opts.attributes, href, token.type);
-			let tagName = options.resolve(opts.tagName, href, token.type);
-			let linkClass = options.resolve(opts.linkClass, href, token.type);
-			let target = options.resolve(opts.target, href, token.type);
-			let events = options.resolve(opts.events, href, token.type);
-
-			let props = {
-				href: href,
-				className: linkClass,
-			};
-
-			if (target) {
-				props.target = target;
-			}
-
-			// Build up additional attributes
-			// Support for events via attributes hash
-			if (attributesHash) {
-				for (var attr in attributesHash) {
-					props[attr] = attributesHash[attr];
-				}
-			}
-
-			elements.push(React.createElement(tagName, props, formatted));
+	for (const token of tokens) {
+		if (token.type === 'nl' && opts.nl2br) {
+			elements.push(React.createElement('br'));
+			continue;
+		} else if (
+			!token.isLink ||
+			!options.resolve(opts.validate, token.toString(), token.type)
+		) {
+			// Regular text
+			elements.push(token.toString());
+			continue;
 		}
 
-		// Span is hardcoded for now
-		return React.createElement('span', {}, ...elements);
+		let href = token.toHref(opts.defaultProtocol);
+		let formatted = options.resolve(opts.format, token.toString(), token.type);
+		let formattedHref = options.resolve(opts.formatHref, href, token.type);
+		let attributesHash = options.resolve(opts.attributes, href, token.type);
+		let tagName = options.resolve(opts.tagName, href, token.type);
+		let linkClass = options.resolve(opts.linkClass, href, token.type);
+		let target = options.resolve(opts.target, href, token.type);
+		let events = options.resolve(opts.events, href, token.type);
+
+		let props = {
+			href: href,
+			className: linkClass,
+		};
+
+		if (target) {
+			props.target = target;
+		}
+
+		// Build up additional attributes
+		// Support for events via attributes hash
+		if (attributesHash) {
+			for (var attr in attributesHash) {
+				props[attr] = attributesHash[attr];
+			}
+		}
+
+		elements.push(React.createElement(tagName, props, formatted));
+	}
+
+	return elements;
+}
+
+// Recursively linkify the contents of the given React Element instance
+function linkifyReactElement(element, opts) {
+	let children = [];
+	React.Children.forEach(element.props.children, (child) => {
+		if (typeof child === 'string') {
+			children.push(...stringToElements(child, opts));
+		} else if (React.isValidElement(child)) {
+			children.push(linkifyReactElement(child, opts));
+		} else {
+			// Unknown element type, just push and skip
+			children.push(element);
+		}
+	});
+	return React.cloneElement(element, element.props, children);
+}
+
+var Linkify = React.createClass({
+	render() {
+		// Copy over all non-linkify-specific props
+		var newProps = {};
+		for (var key in this.props) {
+			if (key !== 'options' && key !== 'tagName') {
+				newProps[key] = this.props[key];
+			}
+		}
+
+		var opts = options.normalize(this.props.options);
+		var tagName = this.props.tagName || 'span';
+		let element = React.createElement(tagName, newProps);
+
+		return linkifyReactElement(element, opts);
 	}
 });
 
--- test/spec/linkify-react-test.js
+++ test/spec/linkify-react-test.js
@@ -3,7 +3,7 @@ const htmlOptions = require('./html/options');
 const extend = require('lodash/extend');
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
-const Linkify = require('../../lib/linkify-react').default;
+const Linkify = require(`${__base}linkify-react`).default;
 
 const options = { // test options
 	tagName: 'em',
@@ -35,31 +35,55 @@ describe('linkify-react', () => {
 		[
 			'Test with no links',
 			'<span>Test with no links</span>',
-			'<span>Test with no links</span>',
+			'<div class="lambda">Test with no links</div>',
 		], [
 			'The URL is google.com and the email is test@example.com',
 			'<span>The URL is <a href="http://google.com" class="linkified" target="_blank">google.com</a> and the email is <a href="mailto:test@example.com" class="linkified">test@example.com</a></span>',
-			'<span>The URL is <em href="https://google.com" class="my-linkify-class" target="_parent" rel="nofollow">google.com</em> and the email is <em href="mailto:test@example.com" class="my-linkify-class" target="_parent" rel="nofollow">test@example.com</em></span>',
+			'<div class="lambda">The URL is <em href="https://google.com" class="my-linkify-class" target="_parent" rel="nofollow">google.com</em> and the email is <em href="mailto:test@example.com" class="my-linkify-class" target="_parent" rel="nofollow">test@example.com</em></div>',
 		], [
 			'Super long maps URL https://www.google.ca/maps/@43.472082,-80.5426668,18z?hl=en, a #hash-tag, and an email: test.wut.yo@gmail.co.uk!\n',
 			'<span>Super long maps URL <a href="https://www.google.ca/maps/@43.472082,-80.5426668,18z?hl=en" class="linkified" target="_blank">https://www.google.ca/maps/@43.472082,-80.5426668,18z?hl=en</a>, a #hash-tag, and an email: <a href="mailto:test.wut.yo@gmail.co.uk" class="linkified">test.wut.yo@gmail.co.uk</a>!\n</span>',
-			'<span>Super long maps URL <em href="https://www.google.ca/maps/@43.472082,-80.5426668,18z?hl=en" class="my-linkify-class" target="_parent" rel="nofollow">https://www.google.ca/maps/@43.472082,-8…</em>, a #hash-tag, and an email: <em href="mailto:test.wut.yo@gmail.co.uk" class="my-linkify-class" target="_parent" rel="nofollow">test.wut.yo@gmail.co.uk</em>!<br/></span>',
+			'<div class="lambda">Super long maps URL <em href="https://www.google.ca/maps/@43.472082,-80.5426668,18z?hl=en" class="my-linkify-class" target="_parent" rel="nofollow">https://www.google.ca/maps/@43.472082,-8…</em>, a #hash-tag, and an email: <em href="mailto:test.wut.yo@gmail.co.uk" class="my-linkify-class" target="_parent" rel="nofollow">test.wut.yo@gmail.co.uk</em>!<br/></div>',
 		]
 	];
 
 	it('Works with default options', function () {
-		tests.map(function (test) {
-			var result = ReactDOMServer.renderToStaticMarkup(React.createElement(Linkify, {data: test[0]}));
+		tests.map((test) => {
+			let linkified = React.createElement(Linkify, null, test[0]);
+			let result = ReactDOMServer.renderToStaticMarkup(linkified);
 			expect(result).to.be.eql(test[1]);
 		});
 	});
 
 	it('Works with overriden options', function () {
-		tests.map(function (test) {
-			var props = extend({data: test[0]}, options);
-			var result = ReactDOMServer.renderToStaticMarkup(React.createElement(Linkify, props));
+		tests.map((test) => {
+			let props = {options, tagName: 'div', className: 'lambda'};
+			let linkified = React.createElement(Linkify, props, test[0]);
+			let result = ReactDOMServer.renderToStaticMarkup(linkified);
 			expect(result).to.be.eql(test[2]);
 		});
 	});
 
+	it('Finds links recursively', function () {
+		let strong = React.createElement(
+			'strong', null, 'https://facebook.github.io/react/'
+		);
+		let linkified = React.createElement(Linkify, null, 'A great site is google.com AND ', strong);
+		let result = ReactDOMServer.renderToStaticMarkup(linkified);
+		expect(result).to.be.eql('<span>A great site is <a href="http://google.com" class="linkified" target="_blank">google.com</a> AND <strong><a href="https://facebook.github.io/react/" class="linkified" target="_blank">https://facebook.github.io/react/</a></strong></span>');
+	});
+
+	it('Excludes self-closing elements', () => {
+		let Delta = React.createClass({
+			render() {
+				return React.createElement(
+					'strong', this.props, 'https://facebook.github.io/react/'
+				);
+			}
+		});
+		let delta = React.createElement(Delta);
+		let linkified = React.createElement(Linkify, null, 'A great site is google.com AND ', delta);
+		let result = ReactDOMServer.renderToStaticMarkup(linkified);
+		expect(result).to.be.eql('<span>A great site is <a href="http://google.com" class="linkified" target="_blank">google.com</a> AND <strong>https://facebook.github.io/react/</strong></span>');
+	});
 });
