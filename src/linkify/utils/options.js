9d8e2f2d0305e1ca11e48445408f8703d3d3a12c
Nick Collings <nick@collingo.com>
2017-02-01 15:02:14 +0000

    Use hasOwnProperty rather than undefined checks

--- src/linkify/utils/options.js
+++ src/linkify/utils/options.js
@@ -23,13 +23,13 @@ function Options(opts) {
 	this.formatHref = opts.formatHref || defaults.formatHref;
 	this.nl2br = opts.nl2br || defaults.nl2br;
 	this.tagName = opts.tagName || defaults.tagName;
-	this.target = opts.target !== undefined ? opts.target : defaults.target;
+	this.target = opts.hasOwnProperty('target') ? opts.target : defaults.target;
 	this.validate = opts.validate || defaults.validate;
 	this.ignoreTags = [];
 
 	// linkAttributes and linkClass is deprecated
 	this.attributes = opts.attributes || opts.linkAttributes || defaults.attributes;
-	this.className = opts.className !== undefined ? opts.className : (opts.linkClass || defaults.className);
+	this.className = opts.hasOwnProperty('className') ? opts.className : (opts.linkClass || defaults.className);
 
 	// Make all tags names upper case
 
