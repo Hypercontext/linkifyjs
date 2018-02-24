b45ee3886df5839471cf15b5e5130a2a99744fb5
Nick Frasser <nfrasser@gmail.com>
2018-02-24 16:28:45 -0500

    Additional text fixes and node compatibility

--- run-tests.sh
+++ run-tests.sh
@@ -2,11 +2,11 @@ node_v=$(node --version)
 
 # Run complete test suite only these conditions hold
 # * SauceLabs credentials are defined
-# * The node version is 4.*
+# * The node version is 6.*
 # * The jQuery version is 1.*
 #	- so only a single member machine in the matrix calls sauce
 
-if [[ $(echo $SAUCE_USERNAME) != "" && $(echo $node_v) == v4.* && $(echo $JQUERY_VERSION) == 1.* ]]; then
+if [[ $(echo $SAUCE_USERNAME) != "" && $(echo $node_v) == v6.* && $(echo $JQUERY_VERSION) == 1.* ]]; then
 	# Run basic and SauceLabs tests
 	echo "Running complete test suite..."
 	npm test || exit 1
