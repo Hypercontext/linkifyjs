b7d420f044e7b49c7b6843e94a70886ca23cbc08
Nick Frasser <nfrasser@gmail.com>
2021-04-03 20:30:31 -0400

    Fix linting rules

--- rollup.config.js
+++ rollup.config.js
@@ -64,7 +64,7 @@ export function linkifyPlugin(name, opts = {}) {
 		{ file: `dist/linkify-plugin-${name}.min.js`, format: 'iife', globals, name: false, plugins: [terser()] }
 	];
 	if (opts.commonjs) {
-		output.push({ file: `lib/plugins/${name}.js`, format: 'cjs', exports: 'auto' })
+		output.push({ file: `lib/plugins/${name}.js`, format: 'cjs', exports: 'auto' });
 	}
 	return {
 		input: `../linkifyjs/src/plugins/${name}.js`,
