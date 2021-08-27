c3672c5f022c9019e312b589340a566fc2460e59
Nick Frasser <nfrasser@gmail.com>
2021-08-26 23:38:50 -0400

    Fix react test

--- test/spec/linkify-react.test.js
+++ test/spec/linkify-react.test.js
@@ -28,19 +28,19 @@ describe('linkify-react', () => {
 	var tests = [
 		[
 			'Test with no links',
-			'<span>Test with no links</span>',
+			'Test with no links',
 			'<div class="lambda">Test with no links</div>',
 		], [
 			'The URL is google.com and the email is test@example.com',
-			'<span>The URL is <a href="http://google.com">google.com</a> and the email is <a href="mailto:test@example.com">test@example.com</a></span>',
+			'The URL is <a href="http://google.com">google.com</a> and the email is <a href="mailto:test@example.com">test@example.com</a>',
 			'<div class="lambda">The URL is <em href="https://google.com" class="my-linkify-class" target="_parent" rel="nofollow">google.com</em> and the email is <em href="mailto:test@example.com?subject=Hello%20from%20Linkify" class="my-linkify-class" target="_parent" rel="nofollow">test@example.com</em></div>',
 		], [
 			'Super long maps URL https://www.google.ca/maps/@43.472082,-80.5426668,18z?hl=en, a #hash-tag, and an email: test.wut.yo@gmail.co.uk!\n',
-			'<span>Super long maps URL <a href="https://www.google.ca/maps/@43.472082,-80.5426668,18z?hl=en">https://www.google.ca/maps/@43.472082,-80.5426668,18z?hl=en</a>, a #hash-tag, and an email: <a href="mailto:test.wut.yo@gmail.co.uk">test.wut.yo@gmail.co.uk</a>!\n</span>',
+			'Super long maps URL <a href="https://www.google.ca/maps/@43.472082,-80.5426668,18z?hl=en">https://www.google.ca/maps/@43.472082,-80.5426668,18z?hl=en</a>, a #hash-tag, and an email: <a href="mailto:test.wut.yo@gmail.co.uk">test.wut.yo@gmail.co.uk</a>!\n',
 			'<div class="lambda">Super long maps URL <em href="https://www.google.ca/maps/@43.472082,-80.5426668,18z?hl=en" class="my-linkify-class" target="_parent" rel="nofollow">https://www.google.ca/maps/@43.472082,-8…</em>, a #hash-tag, and an email: <em href="mailto:test.wut.yo@gmail.co.uk?subject=Hello%20from%20Linkify" class="my-linkify-class" target="_parent" rel="nofollow">test.wut.yo@gmail.co.uk</em>!<br/></div>',
 		], [
 			'Link with @some.username should not work as a link',
-			'<span>Link with @some.username should not work as a link</span>',
+			'Link with @some.username should not work as a link',
 			'<div class="lambda">Link with @some.username should not work as a link</div>',
 		]
 	];
@@ -68,7 +68,7 @@ describe('linkify-react', () => {
 		);
 		var linkified = React.createElement(Linkify, null, 'A great site is google.com AND ', strong);
 		var result = ReactDOMServer.renderToStaticMarkup(linkified);
-		expect(result).to.be.eql('<span>A great site is <a href="http://google.com">google.com</a> AND <strong><a href="https://facebook.github.io/react/">https://facebook.github.io/react/</a></strong></span>');
+		expect(result).to.be.eql('A great site is <a href="http://google.com">google.com</a> AND <strong><a href="https://facebook.github.io/react/">https://facebook.github.io/react/</a></strong>');
 	});
 
 	it('Excludes self-closing elements', () => {
@@ -83,7 +83,7 @@ describe('linkify-react', () => {
 		var delta = React.createElement(Delta);
 		var linkified = React.createElement(Linkify, null, 'A great site is google.com AND ', delta);
 		var result = ReactDOMServer.renderToStaticMarkup(linkified);
-		expect(result).to.be.eql('<span>A great site is <a href="http://google.com">google.com</a> AND <strong>https://facebook.github.io/react/</strong></span>');
+		expect(result).to.be.eql('A great site is <a href="http://google.com">google.com</a> AND <strong>https://facebook.github.io/react/</strong>');
 	});
 
 	it('Obeys ignoreTags option', () => {
@@ -95,7 +95,7 @@ describe('linkify-react', () => {
 		);
 		var linkified = React.createElement(Linkify, {options}, 'A great site is google.com AND ', em);
 		var result = ReactDOMServer.renderToStaticMarkup(linkified);
-		expect(result).to.be.eql('<span>A great site is <a href="http://google.com">google.com</a> AND <em>https://facebook.github.io/react/</em></span>');
+		expect(result).to.be.eql('A great site is <a href="http://google.com">google.com</a> AND <em>https://facebook.github.io/react/</em>');
 
 	});
 });
