19f67da4f936283b1ae2428e50d63f90a4bd60b8
Nick Frasser <nfrasser@gmail.com>
2022-01-20 22:23:24 -0500

    Upgraded CI workflow with publish capabilities

--- test/run.sh
+++ test/run.sh
@@ -2,16 +2,10 @@
 set -e
 
 # Run complete test suite only these conditions hold
-# * SauceLabs credentials are defined
-# * The node version is 14.*
-# * The jQuery version is 1.*
-#	- so only a single member machine in the matrix calls sauce
-
-if [[ "$BROWSERSTACK_ACCESS_KEY" != "" && "$BROWSERSTACK_USERNAME" != "" && "$1" == "16.x" ]]; then
-	# Run build, basic tests and SauceLabs tests
+if [[ "$1" == "--dist" ]]; then
 	echo "Running complete test suite..."
-	npm test
 	npm run lint
+	npm run test:coverage
 	npm run dist:ci
 	npm run test:ci
 	sleep 3  # Wait for threads to exit?
